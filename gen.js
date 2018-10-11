80f8bba83db1c079da60e0370ced97a6d7843bd5
Your Name <you@example.com>
2018-10-11 17:19:42 +0200

    No longer just trees

--- gen.js
+++ gen.js
@@ -58,19 +58,6 @@ function remove_puzzle_from_pool(puzzle){
   }
 }
 
-function get_puzzles_from_output_item(item){
-  let result = []
-  for(let i in item.tags){
-    let tag = item.tags[i]
-    for(let j in puzzles_by_output[tag]){
-      let puzzle =  puzzles_by_output[tag][j]
-      if(result.indexOf(puzzle) === -1){
-        result.push(puzzle)
-      }
-    }
-  }
-  return result
-}
 
 function EscapeRoom(){
   this.nodes = [];
@@ -84,15 +71,69 @@ EscapeRoom.prototype.get_dangling_inputs = function(){
   return [].concat.apply([], inputs);
 }
 
+EscapeRoom.prototype.get_puzzles_from_output_item = function(item, child_puzzle){
+  let result = []
+  for(let i in item.tags){
+    let tag = item.tags[i]
+
+    for(let j in this.nodes){
+      let puzzle = this.nodes[j]
+      if(this.dangling_nodes.indexOf(puzzle) !== -1
+      && puzzle != child_puzzle
+      && (puzzle.max_optional === undefined || puzzle.max_optional > 0)){
+        if(puzzle.optional_outputs && puzzle.optional_outputs.indexOf(tag) !== -1){
+          result.push(puzzle)
+        }
+      }
+    }
+    for(let j in puzzles_by_output[tag]){
+      let puzzle =  puzzles_by_output[tag][j]
+      if(result.indexOf(puzzle) === -1){
+        result.push(puzzle)
+      }
+    }
+  }
+
+  if(this.nodes.length > MAX_PUZZLES){
+    let filtered_result = result.filter((n)=>n.used_in_room || n.inputs ===undefined || n.inputs.length ==  0)
+    console.log("Too big, better filter out things which increase size")
+    if(filtered_result.length > 0){
+      return filtered_result
+    }
+  }
+
+  if(this.nodes.length < MIN_PUZZLES){
+    let filtered_result = result.filter((n)=>
+      !n.used_in_room && n.inputs && n.inputs.length > 0
+    )
+    console.log("Too small, better filter out things which don't increase size")
+    if(filtered_result.length > 0){
+      return filtered_result
+    }
+  }
+
+  return result
+}
+
 EscapeRoom.prototype.to_dotviz = function(){
 
   let item_style = "[shape=diamond fontsize=8 margin=0.03]"
+  let dangling_style = "[color=red fontcolor=red]"
 
   let result = ""
-  result+="digraph G { node [shape=box]"
+  result+="digraph G { node [shape=box];"
+  if(this.nodes.filter(n => !n.inputs || n.inputs.length == 0).length > 0){
+    result+= "start[shape=doublecircle];"
+  }
   for(let i in this.nodes){
     let n = this.nodes[i]
-    result+= '"'+n.name+'";'
+    if(!n.inputs || n.inputs.length == 0){
+      result+= "start-> \""+n.name+"\";"
+    }
+    let style = this.dangling_nodes.indexOf(n) === -1
+                ? ""
+                : dangling_style
+    result+= '"'+n.name+'"'+style+';'
     for(let j in n.input_items){
       let item = n.input_items[j]
       result += '"'+item.name+"\""+item_style+';'
@@ -100,7 +141,6 @@ EscapeRoom.prototype.to_dotviz = function(){
     }
     for(let j in n.output_items){
       let item = n.output_items[j]
-
       result += '"'+n.name+'" -> "'+item.name+'";'
     }
   }
@@ -123,7 +163,7 @@ EscapeRoom.prototype.fix_dangling_node = function(n){
     }
     for(let j in items_by_tag[tag]){
       let item = items_by_tag[tag][j]
-      let outputs = get_puzzles_from_output_item(item)
+      let outputs = this.get_puzzles_from_output_item(item, n)
       for(let k in outputs){
         let puzzle = outputs[k]
         puzzle_options.push({
@@ -138,7 +178,12 @@ EscapeRoom.prototype.fix_dangling_node = function(n){
     }
     remove_item_from_pool(selection.item)
     remove_puzzle_from_pool(selection.puzzle)
-    let puzzle = _.clone(selection.puzzle)
+
+    //Is this a puzzle that's from the list of puzzle_option
+    // Or is it a specific in room puzzle
+    let puzzle = selection.puzzle.used_in_room
+                ? selection.puzzle
+                : _.clone(selection.puzzle)
 
     if(selection.item){
 
@@ -163,38 +208,46 @@ EscapeRoom.prototype.fix_dangling_node = function(n){
 
     }
 
-    if(this.puzzles_by_name[puzzle.name] === undefined){
-      this.puzzles_by_name[puzzle.name] = []
+    if(puzzle.used_in_room){
+      if(puzzle.max_optional){
+        puzzle.max_optional--;
+      }
+    }else{
+      if(this.puzzles_by_name[puzzle.name] === undefined){
+        this.puzzles_by_name[puzzle.name] = []
+      }
+      this.puzzles_by_name[puzzle.name].push(puzzle);
+      if(this.puzzles_by_name[puzzle.name].length > 1){
+        puzzle.name += " № "+(this.puzzles_by_name[puzzle.name].length)
+      }
+      this.nodes.push(puzzle)
+      puzzle.used_in_room = true
+      if(puzzle.inputs && puzzle.inputs.length > 0){
+        this.dangling_nodes.push(puzzle)
+      }
     }
-    this.puzzles_by_name[puzzle.name].push(puzzle);
-    if(this.puzzles_by_name[puzzle.name].length > 1){
-      puzzle.name += " № "+(this.puzzles_by_name[puzzle.name].length)
-    }
-    this.nodes.push(puzzle)
-    if(puzzle.inputs && puzzle.inputs.length > 0){
-      this.dangling_nodes.push(puzzle)
-    }
-
   }
   this.dangling_nodes.splice(this.dangling_nodes.indexOf(n),1)
 }
 
 
+let MIN_PUZZLES = 6
+let MAX_PUZZLES = 12
 let room = new EscapeRoom();
 let door = {name:"Final Door", inputs:["key", "signal"]}
 room.nodes.push(door)
 room.dangling_nodes.push(door)
 try{
+  var viz = new Viz();
   while(room.dangling_nodes.length > 0){
     let index = Math.floor(Math.random()*room.dangling_nodes.length)
     room.fix_dangling_node(room.dangling_nodes[index])
-  }
 
-  var viz = new Viz();
-  viz.renderSVGElement(room.to_dotviz())
-  .then(function(element) {
-    document.body.appendChild(element);
-  });
+    viz.renderSVGElement(room.to_dotviz())
+    .then(function(element) {
+      document.body.appendChild(element);
+    });
+  }
 
 }catch(e){
   console.error(e)
